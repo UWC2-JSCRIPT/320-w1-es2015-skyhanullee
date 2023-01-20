ef5a7a1d51937c2948d85bb4802a63a63cc64bbe
skyhanullee <leeskyha@gmail.com>
2023-01-20 01:58:06 -0800

    initial commit

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,13 @@
 // Import HTMLElement here
+import HTMLElement from './HTMLElement';
 
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super(content);
+    this.content = content;
+  }
+}
 
 // Export class here
+export default { DivElement };
